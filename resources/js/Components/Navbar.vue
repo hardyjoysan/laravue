ffd1f32103a36eb9bfabded85bf4b93a0a0df6b9
Hardy Joyson <hardyjoysan@gmail.com>
2021-04-20 18:31:00 +0530

    Vue front-end integration with vuex and router

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navBar">
+        <ul class="navBar_ul">
+            <li><router-link :to="{ name:'home' }">Home</router-link></li>
+            <li><router-link :to="{ name:'about' }">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'navbar'
+}
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="homepage">
+        <h1>Home Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home'
+}
+</script>
